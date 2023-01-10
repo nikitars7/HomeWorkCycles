5ed07c2c84a0afaa149f5070afc58d7d05676342
Nikita <nikita_rs7@ukr.net>
2023-01-10 21:51:21 +0200

    main

---
+++ first.js
@@ -0,0 +1,82 @@
+'use strict'
+
+const askFunc = () => {
+   let personAge = prompt('Fill in your age', '');
+   let personCity = prompt('What city have u inhabit? =)', '');
+   let personSport = prompt('What is your favourite kind of sport ?', '');
+
+   if (personAge === null) {
+      personAge = 'You didn`t log your age';
+   }
+   else if (personAge.trim() === '') {
+      personAge = 'You didn`t log your age';
+   }
+   else {
+      personAge = `Your age is ${personAge}`;
+   }
+
+   switch (personCity) {
+      case 'Kyiv': {
+         personCity = 'You live in the capital of Ukraine '
+         break;
+      }
+      case 'Washington': {
+         personCity = 'You live in the capital of USA '
+         break;
+      }
+      case 'London': {
+         personCity = 'You live in the capital of GreatBritain '
+         break;
+      }
+      case null: {
+         personCity = 'You didn`t log your city'
+         break;
+      }
+      case '': {
+         personCity = 'You didn`t log your city'
+         break;
+      }
+      default: {
+         personCity = `You live in ${personCity}`
+         break;
+      }
+   }
+
+   switch (personSport) {
+      case 'strongman': {
+         personSport = 'Great! Do you want to be like Oleksei Novikov?';
+         break;
+      }
+      case 'boxing': {
+         personSport = 'Great! Do you want to be like Usik?';
+         break;
+      }
+      case 'armwrestling': {
+         personSport = 'Great! Do you want to be like Levan ?';
+         break;
+      }
+      case null: {
+         personSport = 'You didn`t log your favourite Sport';
+         break;
+      }
+      case '': {
+         personSport = 'You didn`t log your favourite Sport';
+         break;
+      }
+      default: {
+         personSport = `${personSport} is so exciting`;
+         break;
+      }
+   }
+
+   alert(`
+   ${personAge}
+   ${personCity}
+   ${personSport}`
+   );
+}
+
+askFunc();
+
+
+
